1f9724e664f7fa01d1732e66a3a537b5d416a47f
Abhi22012002 <abhijeetshelar70@gmail.com>
2023-11-04 23:55:54 +0530

    Notes data and date and time saved in localstorage

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,27 +1,45 @@
-import React from 'react';
-import './Homepage.css';
-import { useNoteContext } from './NoteContext';
+import React, { useState } from "react";
+import "./Homepage.css";
+import { useNoteContext } from "./NoteContext";
 
 const Homepage = () => {
   const { displayTitle } = useNoteContext();
+  const [date,setDate]=useState(new Date());
+  const [noteNamesParent, setNoteNamesParent] = useState(
+    JSON.parse(localStorage.getItem(displayTitle)) || []
+  );
+
+  const [noteText, setNoteText] = useState("");
+  const handleChange = (e) => {
+    setNoteText(e.target.value);
+  };
+  const handleClick = () => {
+     setDate(new Date());
+    const newGroup = { note:noteText, date:date};
+    const updatedNoteNames = [...noteNamesParent, newGroup];
+    setNoteNamesParent(updatedNoteNames);
+   localStorage.setItem(displayTitle,JSON.stringify(updatedNoteNames));
+   setNoteText('');
+
+  };
 
   return (
-    <div className='home-container'>
-      <div className='header-note'>
+    <div className="home-container">
+      <div className="header-note">
         <h1>{displayTitle}</h1>
       </div>
-      <div className='display-notes'>
-
-      </div>
-      <div className='type-container'>
+      <div className="display-notes"></div>
+      <div className="type-container">
         <input
-          type='text'
-          id='note-text'
-          className='note-box'
+          type="text"
+          id="note-text"
+          className="note-box"
           required
-          placeholder='Enter your text here...........'
+          placeholder="Enter your text here..........."
+          value={noteText}
+          onChange={handleChange}
         ></input>
-        <button className='send'></button>
+        <button className="send" onClick={handleClick}></button>
       </div>
     </div>
   );
