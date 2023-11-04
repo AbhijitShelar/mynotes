86bc2a582d5ed79a1afa849199fee5e8591d0ea0
Abhi22012002 <abhijeetshelar70@gmail.com>
2023-11-04 19:38:42 +0530

    Note input design added

--- src/CreateNote.js
+++ src/CreateNote.js
@@ -1,45 +1,54 @@
-import React, { useState } from "react";
-import { useNoteContext } from "./NoteContext";
-import "./CreateNote.css";
+import React, { useState } from 'react';
+import { useNoteContext } from './NoteContext';
+import './CreateNote.css';
 
 const CreateNote = () => {
-  const [groupName, setGroupName] = useState("");
+  const [groupName, setGroupName] = useState('');
   const { addTitle } = useNoteContext();
 
   const handlechange = (e) => {
     setGroupName(e.target.value);
   };
-  const isInputEmpty = groupName.trim() === "";
+
   const handleclick = () => {
-    if(isInputEmpty){
-            alert('Enter a Group name');
+    if (!groupName) {
+      alert('Enter a Group name');
+      return;
     }
-    else{
+  
     const hide = document.getElementById("root-container");
     hide.style.display = "none";
+  
+    // Retrieve existing group names from localStorage
+    
+    // Store the updated array back in localStorage
     localStorage.setItem(groupName, groupName);
+  
+    // Update your state or context to reflect the change
     addTitle(groupName);
-    setGroupName("");
-    }
+  
+    setGroupName(''); // Clear the input field
   };
+  
+
   return (
-    <div id="root-container">
+    <div id='root-container'>
       <h1>Create New Notes group</h1>
-      <div className="group-name">
+      <div className='group-name'>
         <h2>Group name</h2>
         <input
-          id="input-text"
-          type="text"
-          className="input-name"
+          id='input-text'
+          type='text'
+          className='input-name'
           value={groupName}
           onChange={handlechange}
-          placeholder="Enter Group Name...."
+          placeholder='Enter Group Name....'
         ></input>
       </div>
-      <div className="color">
+      <div className='color'>
         <h3>Choose Color</h3>
       </div>
-      <button id="create-btn" className="create-button" onClick={handleclick} >
+      <button id='create-btn' className='create-button' onClick={handleclick}>
         Create
       </button>
     </div>
--- src/Homepage.js
+++ src/Homepage.js
@@ -1,16 +1,27 @@
+import React from 'react';
 import './Homepage.css';
 import { useNoteContext } from './NoteContext';
+
 const Homepage = () => {
-       const {displayTitle}=useNoteContext();      
+  const { displayTitle } = useNoteContext();
 
   return (
     <div className='home-container'>
       <div className='header-note'>
         <h1>{displayTitle}</h1>
       </div>
-     
+      <div className='type-container'>
+        <input
+          type='text'
+          id='note-text'
+          className='note-box'
+          required
+          placeholder='Enter your text here...........'
+        ></input>
+        <button className='send'></button>
+      </div>
     </div>
   );
-}
+};
 
 export default Homepage;
